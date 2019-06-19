3654a83d36e9c25e933c173aac8c3e79c72b9f01
Tomoaki Ubuntu <tomoaki.takaichi01@g.softbank.co.jp>
2019-06-19 15:36:08 +0900

    eureka ribbon clientサンプル追加

--- eureka-requester-sample/src/main/java/com/syamozi/sample/requester/eureka/eurekarequestersample/EurekaRequesterSampleApplication.java
+++ eureka-requester-sample/src/main/java/com/syamozi/sample/requester/eureka/eurekarequestersample/EurekaRequesterSampleApplication.java
@@ -1,20 +1,31 @@
 package com.syamozi.sample.requester.eureka.eurekarequestersample;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
 @RestController
+@RibbonClient(name = "sayhello", configuration = SayHelloConfiguration.class)
 public class EurekaRequesterSampleApplication {
 
-  RestTemplate restTemplate = new RestTemplate();
+  @LoadBalanced
+  @Bean
+  RestTemplate restTemplate(){
+    return new RestTemplate();
+  }
+
+  @Autowired
+  RestTemplate restTemplate;
   
-  private String url = "http://localhost:8081";
+  private String url = "http://sayhello";
   
   @RequestMapping(value = "/")
   public String home() {
---
+++ eureka-requester-sample/src/main/java/com/syamozi/sample/requester/eureka/eurekarequestersample/SayHelloConfiguration.java
@@ -0,0 +1,26 @@
+package com.syamozi.sample.requester.eureka.eurekarequestersample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingUrl;
+import com.netflix.loadbalancer.AvailabilityFilteringRule;
+
+public class SayHelloConfiguration {
+  @Autowired
+  IClientConfig ribbonClientConfig;
+
+  @Bean
+  public IPing ribbonPing(IClientConfig config) {
+    return new PingUrl();
+  }
+
+  @Bean
+  public IRule ribbonRule(IClientConfig config) {
+    return new AvailabilityFilteringRule();
+  }
+
+}
